705b1a3bd831565415e63d77433494a56b28c4fe
felipecosta <fcosta.paltrinieri@gmail.com>
2025-05-14 19:53:07 -0300

    Validar contrato com anotações

--- src/main/java/com/aula/agendaTelefonica/ContatoEntradaDTO.java
+++ src/main/java/com/aula/agendaTelefonica/ContatoEntradaDTO.java
@@ -1,14 +1,20 @@
 package com.aula.agendaTelefonica;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class ContatoEntradaDTO {
 
-    @NotEmpty
+    @NotEmpty(message = "Por favor, informe seu nome completo")
+    @Size(min = 5, max = 20, message = "Nome deve ter entre 5 e 20 caracteres")
     private String nome;
 
     @NotNull
+    @Max(value = 99, message = "Número maximo de caracteres permitido 2")
+    @Min(value = 11, message = "Número mínimo de caracteres permitidos 2")
     private Integer dddCelular;
 
     @NotNull
